3bc997c3a656b407384861280efed12b337ac853
na-x-ril <nazrilpro10@gmail.com>
2025-07-27 01:06:55 +0700

    fix: missing microformat

--- api/yt/v-get.js
+++ api/yt/v-get.js
@@ -1,20 +1,21 @@
+import { sendJson } from '../../utils/sendJson.js'
 import { fetchYouTubeData } from '../../utils/parser.js';
 import { getSponsorSegments } from '../../utils/sponsorblock.js';
 
 export default async function handler(req, res) {
-  const { videoId } = req.query;
+  const { videoUrl } = req.query;
 
-  if (!videoId) return res.status(400).json({ error: 'Missing videoId' });
+  if (!videoUrl) return res.status(400).json({ error: 'Missing videoUrl' });
 
   try {
-    const data = await fetchYouTubeData(videoId);
-    const sponsorSegments = await getSponsorSegments(videoId);
+    const data = await fetchYouTubeData(videoUrl);
+    const sponsorSegments = await getSponsorSegments(videoUrl);
 
-    res.json({
-      videoId,
+    sendJson( res, 200, {
+      videoUrl,
       ...data,
       sponsorSegments
-    });
+    })
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Failed to fetch or parse data' });
